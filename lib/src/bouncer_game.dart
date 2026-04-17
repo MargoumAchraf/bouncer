c833f4241d641d61db2d517318f474970fd2be91
amargoum <achrafmargouma@gmail.com>
2026-04-17 12:37:43 +0100

    do map

--- lib/src/bouncer_game.dart
+++ lib/src/bouncer_game.dart
@@ -1,6 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/scheduler.dart';
 import 'package:sensors_plus/sensors_plus.dart';
-import 'dart:async';
+
+import 'package:bouncer/src/model.dart';
 
 class BouncerGame extends StatefulWidget {
   const BouncerGame({super.key, this.accelerometerStream});
@@ -13,99 +15,135 @@ class BouncerGame extends StatefulWidget {
 
 class _BouncerGameState extends State<BouncerGame>
     with SingleTickerProviderStateMixin {
-  late AnimationController _controller;
+  List<Block> _blocks = [];
 
-  double _ballX = 0.0;
-  double _ballY = 0.0;
-  double _ballVelocityX = 0.0;
-  double _ballVelocityY = 0.0;
+  Offset _ballPosition = const Offset(200, 400);
+  Offset _ballVelocity = const Offset(170, -200);
 
-  final double _ballRadius = 20.0;
-  final double _gravity = 0.5;
-  final double _damping = 0.8;
-
-  double _screenWidth = 0.0;
-  double _screenHeight = 0.0;
-
-  late StreamSubscription<AccelerometerEvent> _accelerometerSubscription;
+  final double _ballRadius = 10;
+  double _paddleX = 0;
+  double get _paddleTop => MediaQuery.of(context).size.height - 80;
+  late Ticker _ticker;
 
   @override
   void initState() {
     super.initState();
 
-    // 🎮 Animation loop
-    _controller = AnimationController(
-      vsync: this,
-      duration: const Duration(milliseconds: 16),
-    )..addListener(_updatePhysics);
+    _ticker = createTicker((elapsed) {
+      _updateBall(1 / 60);
+    });
 
-    _controller.repeat();
+    _ticker.start();
+  }
 
-    // 📱 Accelerometer
-    _accelerometerSubscription =
-        (widget.accelerometerStream ?? accelerometerEvents)
-            .listen((AccelerometerEvent event) {
-      _ballVelocityX += event.x * 0.2;
-      _ballVelocityY += event.y * 0.2;
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+    _generateBlocks();
+  }
+
+  void _updateBall(double dt) {
+    final screenSize = MediaQuery.of(context).size;
+
+    double x = _ballPosition.dx + _ballVelocity.dx * dt;
+    double y = _ballPosition.dy + _ballVelocity.dy * dt;
+
+    // 🧱 collision مع الحواف
+    if (x <= 0 || x >= screenSize.width - _ballRadius * 2) {
+      _ballVelocity = Offset(-_ballVelocity.dx, _ballVelocity.dy);
+    }
+
+    if (y <= 0) {
+      _ballVelocity = Offset(_ballVelocity.dx, -_ballVelocity.dy);
+    }
+
+    setState(() {
+      _ballPosition = Offset(x, y);
     });
   }
 
-  void _updatePhysics() {
-    setState(() {
-      _ballVelocityY += _gravity;
+  void _generateBlocks() {
+    final screenWidth = MediaQuery.of(context).size.width;
 
-      _ballX += _ballVelocityX;
-      _ballY += _ballVelocityY;
+    final blockWidth =
+        (screenWidth - GameConfig.blockInset * 2) / GameConfig.columns;
 
-      // 🧱 collision مع الحواف
+    final blocks = <Block>[];
 
-      // left & right
-      if (_ballX <= 0) {
-        _ballX = 0;
-        _ballVelocityX = -_ballVelocityX * _damping;
-      } else if (_ballX >= _screenWidth - _ballRadius * 2) {
-        _ballX = _screenWidth - _ballRadius * 2;
-        _ballVelocityX = -_ballVelocityX * _damping;
+    for (int row = 0; row < GameConfig.rows; row++) {
+      for (int col = 0; col < GameConfig.columns; col++) {
+        final left = GameConfig.blockInset + col * blockWidth;
+        final top = 60 + row * (GameConfig.blockHeight + GameConfig.blockGap);
+
+        blocks.add(
+          Block(
+            rect: Rect.fromLTWH(
+              left,
+              top,
+              blockWidth - GameConfig.blockGap,
+              GameConfig.blockHeight,
+            ),
+            color: rowColor(row),
+          ),
+        );
       }
+    }
 
-      // top & bottom
-      if (_ballY <= 0) {
-        _ballY = 0;
-        _ballVelocityY = -_ballVelocityY * _damping;
-      } else if (_ballY >= _screenHeight - _ballRadius * 2) {
-        _ballY = _screenHeight - _ballRadius * 2;
-        _ballVelocityY = -_ballVelocityY * _damping;
-      }
-
-      // friction
-      _ballVelocityX *= 0.99;
-      _ballVelocityY *= 0.99;
-    });
+    _blocks = blocks;
   }
 
   @override
   void dispose() {
-    _controller.dispose();
-    _accelerometerSubscription.cancel();
+    _ticker.dispose();
     super.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    _screenWidth = MediaQuery.of(context).size.width;
-    _screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
+      backgroundColor: Colors.black,
       body: Stack(
         children: [
+          // blocks
+          ..._blocks.map((block) {
+            return Positioned(
+              left: block.rect.left,
+              top: block.rect.top,
+              width: block.rect.width,
+              height: block.rect.height,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: block.color,
+                  borderRadius: BorderRadius.circular(6),
+                ),
+              ),
+            );
+          }),
+
           Positioned(
-            left: _ballX,
-            top: _ballY,
+            left: _paddleX,
+            top: _paddleTop,
+            child: Container(
+              width: GameConfig.paddleWidth,
+              height: GameConfig.paddleHeight,
+              decoration: BoxDecoration(
+                color: const Color(0xFF4DD0E1),
+                borderRadius: BorderRadius.circular(
+                  GameConfig.paddleHeight / 2,
+                ),
+              ),
+            ),
+          ),
+
+          // الكرة ⚽
+          Positioned(
+            left: _ballPosition.dx,
+            top: _ballPosition.dy,
             child: Container(
               width: _ballRadius * 2,
               height: _ballRadius * 2,
               decoration: const BoxDecoration(
-                color: Colors.cyan,
+                color: Colors.white,
                 shape: BoxShape.circle,
               ),
             ),
